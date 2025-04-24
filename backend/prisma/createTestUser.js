4b9d2105d4974383f68f13824409c62c60101242
biancacomotto <bcomotto@mail.austral.edu.ar>
2025-04-24 14:53:57 -0300

    merge

---
+++ backend/prisma/createTestUser.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcryptjs');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function createTestUser() {
+    const hashedPassword = await bcrypt.hash('1234', 10); // o la contraseña que vayas a probar
+
+    const user = await prisma.user.create({
+        data: {
+            email: 'chefcito@example.com',
+            username: 'chefcito',
+            password: hashedPassword
+        }
+    });
+
+    console.log('Usuario creado:', user);
+}
+
+createTestUser();
